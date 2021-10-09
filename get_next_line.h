fffbf881ce3dba2f613b67063f07d75d75db5485
Spuckler Otherys <sotherys@student.21-school.ru>
2021-10-09 14:03:19 +0300

    pre evaluation state

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sotherys <sotherys@student.21-school.ru>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/07 17:07:26 by sotherys          #+#    #+#             */
+/*   Updated: 2021/10/08 19:03:44 by sotherys         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 128
+# endif
+
+# define FD_SIZE 12000
+
+char	*get_next_line(int fd);
+
+char	*ft_strchr_gnl(const char *s, int c, size_t *offset);
+char	*ft_strjoin_gnl(char const *s1, char const *s2, \
+						size_t len1, size_t len2);
+char	*ft_substr_gnl(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *s);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sotherys <sotherys@student.21-school.ru>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/08 04:03:45 by sotherys          #+#    #+#             */
+/*   Updated: 2021/10/08 04:03:47 by sotherys         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		++i;
+	return (i);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (!dst && !src)
+		return (dst);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *) dst)[i] = ((unsigned char *) src)[i];
+		++i;
+	}
+	return (dst);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	i;
+	char	*new;
+
+	new = (char *) malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		new[i] = s1[i];
+		++i;
+	}
+	new[i] = '\0';
+	return (new);
+}
